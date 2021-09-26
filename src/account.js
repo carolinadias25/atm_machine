8c31a68dd9d516630aa85baaf1a88221caacbc75
carolinadias25 <carolina.f.dias@hotmail.com>
2021-09-26 19:47:34 +0100

    adding printInput function

--- src/account.js
+++ src/account.js
@@ -1,18 +1,17 @@
 class Account {
-  constructor(number, pin, amount, overdraft) {
-    console.log(number, pin); //printing out input accNumber and pin
-    console.log(amount, overdraft); //printing out input amount and over draft input
+  constructor(number, pin, balanceAmount, overdraft) {
     this.accNumber = number;
     this.pin = pin;
     this.overdraft = overdraft;
-    this.balance = amount;
+    this.balance = balanceAmount;
   }
   withdraw(pin, amount) {
     console.log(`W ${amount}\n----`); //printing out W and the withdrawal amount
     if (this.wrongPinIsProvided(pin)) {
       return { status: "error", message: "ACCOUNT_ERR" };
     } else if (this.balance + this.overdraft < amount) {
-      return { status: "error", message: "FOUNDS_ERR" };
+      // if the amount is greater than the balance and the overdraft, return funds_err
+      return { status: "error", message: "FUNDS_ERR" };
     } else {
       this.balance -= amount;
       console.log(`B ${this.balance}\n----`); //printing out B and the balance amount
@@ -20,14 +19,19 @@ class Account {
   }
 
   wrongPinIsProvided(pin) {
-    return this.pin !== pin;
+    return this.pin !== pin; // check if pin matches the correct this.pin
   }
   getBalance(pin) {
     if (this.wrongPinIsProvided(pin)) {
-      return { status: "error", message: "ACCOUNT_ERR" };
+      return { status: "error", message: "ACCOUNT_ERR" }; //only gets access to balance if pin is correct
     } else {
       console.log(`B ${this.balance}\n----`); //printing out B and the balance amount
+      return this.balance;
     }
+  }
+  printInput() {
+    let printOut = `${this.accNumber} ${this.pin}\n${this.balance} ${this.overdraft}\n`; //printing out input accNumber, pin, balanceAmount and overdraft input
+    return printOut;
   }
 }
 
--- tests/account.test.js
+++ tests/account.test.js
@@ -7,9 +7,9 @@ describe("Account class", () => {
     correctPinCode = "1234";
     wrongPinCode = "0000";
     overdraft = 100;
-    amount = 200;
+    balanceAmount = 200;
     withdrawAmount = 50;
-    account = new Account(accNumber, correctPinCode, amount, overdraft);
+    account = new Account(accNumber, correctPinCode, balanceAmount, overdraft);
   });
 
   it("is expected to be initiased", () => {
@@ -19,31 +19,19 @@ describe("Account class", () => {
     expect(account).to.be.instanceOf(Account);
   });
   it("is expected to hold an amount when initialized", () => {
-    expect(account.getBalance(correctPinCode)).to.equal(
-      console.log(`B ${amount}\n----`)
-    );
+    expect(account.getBalance(correctPinCode)).to.equal(balanceAmount);
   });
   it("is expected to reduce balance on sucessful withdraw", () => {
     account.withdraw(correctPinCode, withdrawAmount);
-    expect(account.getBalance(correctPinCode)).to.equal(
-      console.log(`B ${amount}\n----`)
-    );
-  });
-  it("is expected return an input printed", () => {
-    account.withdraw(wrongPinCode, withdrawAmount);
-    expect(account.withdraw(correctPinCode, withdrawAmount)).to.equal(
-      console.log(`W ${withdrawAmount}\n----`)
-    );
+    expect(account.getBalance(correctPinCode)).to.equal(150);
   });
   describe("The Account should not dispense funds if the pin is incorrect", () => {
     it("is expected NOT to reduce account amount", () => {
       account.withdraw(wrongPinCode, 50);
-      expect(account.getBalance(correctPinCode)).to.equal(
-        console.log(`B ${amount}\n----`)
-      );
+      expect(account.getBalance(correctPinCode)).to.equal(balanceAmount);
     });
     it("is expected to return error status and message", () => {
-      expect(account.withdraw(wrongPinCode, 50)).to.eql({
+      expect(account.withdraw(wrongPinCode, withdrawAmount)).to.eql({
         status: "error",
         message: "ACCOUNT_ERR",
       });
@@ -53,7 +41,7 @@ describe("Account class", () => {
     it("is expected to return error status and message", () => {
       expect(account.withdraw(correctPinCode, 800)).to.eql({
         status: "error",
-        message: "FOUNDS_ERR",
+        message: "FUNDS_ERR",
       });
     });
     describe("Cannot get balance if pin is incorrect", () => {
@@ -64,9 +52,10 @@ describe("Account class", () => {
         });
       });
       it("is expected to return the balance if the pin is correct", () => {
-        expect(account.getBalance(correctPinCode)).to.equal(
-          console.log(`B ${amount}\n----`)
-        );
+        expect(account.getBalance(correctPinCode)).to.equal(balanceAmount);
+      });
+      it("is expected print the account number, pin, balance and overdraft", () => {
+        expect(account.printInput()).to.equal("12345678 1234\n200 100\n");
       });
     });
   });
